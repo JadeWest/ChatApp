c1dd528c02eec5b3505351d143acd046bc537be6
hbc8141 <shng93@outlook.com>
2021-06-29 13:14:48 +0900

    first commit

---
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 서현규 on 2021/06/17.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ConversationsViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+    }
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ChatApp' do
+  use_frameworks!
+
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+
+#Google Sign In
+pod 'GoogleSignIn'
+
+pod 'MessageKit'
+pod 'JGProgressHUD'
+pod 'RealmSwift'
+pod 'SDWebImage'
+
+end
